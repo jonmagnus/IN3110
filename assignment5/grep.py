357c4203d42c6b01b9a67b03f9ae49913adecbd9
Jon-Magnus Rosenbad <jonmagnusr@gmail.com>
2019-10-14 17:41:12 +0200

    Add grep with single color highlight

---
+++ assignment5/grep.py
@@ -0,0 +1,40 @@
+import re
+import argparse
+import os
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+            description='A grep clone implemented using python regex.')
+    parser.add_argument(
+            'sourcefile',
+            type=str,
+            help='The file to search for occurences of the regex in.')
+    parser.add_argument(
+            'regex',
+            type=str,
+            help='The regex to search for in the sourcefile.')
+    parser.add_argument(
+            '--highlight',
+            action='store_true',
+            help='Set if turning highlighting on.')
+    args = parser.parse_args()
+    assert os.path.exists(args.sourcefile), 'There is no sourcefile at \'{args.sourcefile}\'.'
+    return args
+    
+if __name__ == '__main__':
+    args = parse_arguments()
+    notheme = '\033[0m'
+    color = '\033[31m'
+    
+    with open(args.sourcefile) as infile:
+        for line in infile:
+            match = re.search(args.regex,line)
+            if match:
+                if args.highlight is not None:
+                    line = line[:match.start(0)] \
+                           + color \
+                           + line[match.start(0):match.end(0)] \
+                           + notheme \
+                           + line[match.end(0):]
+                print(line)
