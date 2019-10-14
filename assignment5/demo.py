78754e11e631f9fc86c2d613234fccba5e3dd34b
Jon-Magnus Rosenbad <jonmagnusr@gmail.com>
2019-10-14 17:19:51 +0200

    Add python syntax highlighting files

---
+++ assignment5/demo.py
@@ -0,0 +1,11 @@
+# A comment
+# A comment with some other syntax in: for i in range def function()
+
+import matplotlib.pyplot as plt
+from a import b as c
+
+if False or True is not None:
+    print(Some fancy stuff)
+
+def a(123):
+    pass
