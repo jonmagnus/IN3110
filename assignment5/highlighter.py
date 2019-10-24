697ca7fa440a746a072438e76c5176f6f81aa039
Jon-Magnus Rosenbad <jonmagnusr@gmail.com>
2019-10-24 20:59:32 +0200

    Moved syntax highlighting to separate methods.

--- assignment5/highlighter.py
+++ assignment5/highlighter.py
@@ -34,6 +34,38 @@ def parse_arguments():
         f'There is no sourcefile at \'{args.sourcefile}\'.'
     return args
 
+def mark_syntax(line, syntax, priority={}):
+    priority = {value: 0 if value not in priority else priority[value] \
+        for value in theme}
+    priority[None] = -1
+    color = [None]*len(line)
+    
+    for regex in syntax:
+        value = syntax[regex]
+        for match in re.finditer(regex, line, re.MULTILINE):
+            groups = [0]
+            if match.groups():
+                groups = [i + 1 for i,_ in enumerate(match.groups())]
+            for g in groups:
+                print('MATCH',value,line[match.start(g):match.end(g)],'in',line[match.start(0):match.end(0)])
+                for i in range(match.start(g),match.end(g)):
+                    if priority[value] > priority[color[i]]:
+                        color[i] = value
+    
+    return line, color
+
+def color_line(line, color, colormap):
+    pre_value = None
+    offset = 0
+    for i,value in enumerate(color):
+        if value == pre_value:
+            continue
+        new_line = line[:i + offset] + colormap[value] + line[i + offset:]
+        offset += len(colormap[value])
+        line = new_line
+        pre_value = value
+    
+    return line
 
 if __name__ == '__main__':
     args = parse_arguments()
@@ -65,69 +97,14 @@ if __name__ == '__main__':
             color = delim.join(line.split(delim)[1:])
             theme[value] = color
 
-    priority = {value: 0 if value not in priority else priority[value] \
-        for value in theme}
-    priority[None] = -1
-
-    regtheme = {regex: '\033[{}m'.format(theme[syntax[regex]])
-                for regex in syntax}
     notheme = '\033[0m'
-
     colormap = {value: f'\033[{theme[value]}m' for value in theme}
     colormap = {**colormap, None: notheme}
 
     with open(args.sourcefile, 'r') as infile:
         lines = [l for l in infile]
         line = ''.join(lines)
-        
-        '''
-        is_used = '0'*len(line)
-        for regex in regtheme:
-            offset = 0
-            for match in re.finditer(regex, line, re.MULTILINE):
-                g = 0
-                if match.groups():
-                    g = 1
-                # If the regoin has already been matched by some regex.
-                print('POSSIBLE MATCH', syntax[regex],regtheme[regex] + line[match.start(g) + offset:match.end(g) + offset] + notheme,'in',line[match.start(0) + offset:match.end(g) + offset])
-                if '1' in is_used[match.start(g) + offset:match.end(g) + offset]:
-                    continue
-                print('MATCH')
 
-                new_line = line[:match.start(g) + offset] \
-                    + regtheme[regex] \
-                    + line[match.start(g) + offset:match.end(g) + offset] \
-                    + notheme \
-                    + line[match.end(g) + offset:]
-                len_diff = len(new_line) - len(line)
-                is_used = is_used[:match.start(g) + offset] \
-                    + '1'*(len_diff + match.end(g) - match.start(g)) \
-                    + is_used[match.end(g) + offset:]
-                offset += len_diff
-                line = new_line
-        '''
-
-        color = [None]*len(line)
-        
-        for regex in syntax:
-            value = syntax[regex]
-            for match in re.finditer(regex, line, re.MULTILINE):
-                groups = [0]
-                if match.groups():
-                    groups = [i + 1 for i,_ in enumerate(match.groups())]
-                for g in groups:
-                    print('MATCH',value,line[match.start(g):match.end(g)],'in',line[match.start(0):match.end(0)])
-                    for i in range(match.start(g),match.end(g)):
-                        if priority[value] > priority[color[i]]:
-                            color[i] = value
-        
-        pre_value = None
-        offset = 0
-        for i,value in enumerate(color):
-            if value == pre_value:
-                continue
-            new_line = line[:i + offset] + colormap[value] + line[i + offset:]
-            offset += len(colormap[value])
-            line = new_line
-            pre_value = value
-        print(line)
+    line, color = mark_syntax(line, syntax, priority)
+    line = color_line(line, color, colormap)
+    print(line)
