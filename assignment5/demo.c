8146dd0988d155f3895a3ef33e9eb84a0e000060
Jon-Magnus Rosenbad <jonmagnusr@gmail.com>
2019-10-29 10:29:04 +0100

    Add syntax coloring for c

---
+++ assignment5/demo.c
@@ -0,0 +1,18 @@
+// A normal comment
+/*
+ * A multiline comment
+ */ 
+
+#include <stdio.h>
+#include "foo.h"
+
+struct Node {
+	Node *prev, *next;
+	int value;
+};
+
+int main() {
+	printf("Hello world");
+	char a = 'w';
+	return 0;
+}
